6bf2bff200e7980b8e08e585b61741ca5048f8f6
Tania <taniameoow@gmail.com>
2020-02-07 09:20:28 +0200

    Updates

---
+++ precache-manifest.0d6ba797b9d0d182bdbb54a724909937.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515f906768f7e340d94044cc495f8358",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "89750f39441fc547053f",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.4087c797.chunk.css"
+  },
+  {
+    "revision": "fe7d8e20142d1250fb67",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c2efa7c8.chunk.css"
+  },
+  {
+    "revision": "89750f39441fc547053f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ffbaddb0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ffbaddb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7d8e20142d1250fb67",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.9f43eebd.chunk.js"
+  },
+  {
+    "revision": "aa5f66c794a5306fc6ea",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.12b1a04c.js"
+  }
+]);
